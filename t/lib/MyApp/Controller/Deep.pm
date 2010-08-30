8c3f88ee3bc7f376e891db9cba670a257fc68379
Zbigniew Lukasiak <zby@cpan.org>
2010-08-30 19:42:24 +0200

    controllers for tests

---
+++ t/lib/MyApp/Controller/Deep.pm
@@ -0,0 +1,8 @@
+use strict;
+use warnings;
+
+package MyApp::Controller::Deep;
+
+use base 'WebNano::Controller';
+
+1;
---
+++ t/lib/MyApp/Controller/Deep/Nested.pm
@@ -0,0 +1,10 @@
+use strict;
+use warnings;
+
+package MyApp::Controller::Deep::Nested;
+
+use base 'WebNano::Controller';
+
+sub some_action { "This is 'some_action' in 'MyApp::Controller::Deep::Nested'" }
+
+1;
