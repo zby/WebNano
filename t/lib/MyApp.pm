248d650485ccc1e070e05e8f3cdb7ad039966768
Zbigniew Lukasiak <zby@cpan.org>
2010-09-09 22:20:43 +0200

    removing dependency on Config modules

--- t/lib/MyApp.pm
+++ t/lib/MyApp.pm
@@ -3,22 +3,16 @@ use warnings;
 
 package MyApp;
 use base 'WebNano';
-use Object::Tiny::RW 'config';
-use Config::Any;
 use WebNano::Renderer::TTiny;
 
 sub new {
     my $class = shift;
     my $self  = $class->SUPER::new( @_ );
-    my $cfg = Config::Any->load_stems({ stems => [ $self->config_file ], use_ext => 1 }); 
-    my @values = values %{$cfg->[0]};
-    my $config = $values[0];
-    $self->config( $config );
-    $self->renderer( WebNano::Renderer::TTiny->new( %{ $config->{renderer} } ) );
+    $self->renderer( WebNano::Renderer::TTiny->new( $self->renderer_config() ) );
     return $self;
 }
 
-sub config_file { 't/data/app' }
+sub renderer_config { root => 't/data/templates' }
 
 1;
 
--- t/lib/SubClassApp.pm
+++ t/lib/SubClassApp.pm
@@ -4,7 +4,7 @@ use warnings;
 package SubClassApp;
 use base 'MyApp';
 
-sub config_file { 't/data/subclassapp' } 
+sub renderer_config { root => [ 't/data/subclassapp_templates', 't/data/templates' ] }
 
 sub controller_search_path { [ ref(shift), 'MyApp' ] };
 
